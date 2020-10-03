06274b3cc1ab0d178b4f636f73a5a20fd8c7c323
ryanzhangau <ryanzhangau@gmail.com>
2020-10-04 10:27:30 +1100

    init

---
+++ precache-manifest.57efeaab1a52747f0354da043cfbf492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4460f19bb3d1fc0a02cb1c007eeee934",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bb2da46699aac8b447ea.bundle.js"
+  },
+  {
+    "url": "runtime~main.bb2da46699aac8b447ea.bundle.js"
+  },
+  {
+    "revision": "5c5515d660cfdf048ce2",
+    "url": "static/css/vendors~main.f3cffc9e.chunk.css"
+  },
+  {
+    "url": "vendors~main.bb2da46699aac8b447ea.bundle.js"
+  },
+  {
+    "url": "vendors~main.bb2da46699aac8b447ea.bundle.js.LICENSE.txt"
+  }
+]);
